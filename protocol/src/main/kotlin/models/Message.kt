b72bcdf72a604716a2ee1aa94389c79a490d3d98
Tom Daffurn <tdaffurn@tbd.email>
2023-09-29 14:36:34 +1000

    Tom's Message and Resource models

--- protocol/src/main/kotlin/models/Message.kt
+++ protocol/src/main/kotlin/models/Message.kt
@@ -1,31 +1,30 @@
 package models
 
-abstract class Message<T: MessageType>(
+class Message<T: MessageData>(
   val metadata: MessageMetadata,
-  val data: MessageData<T>,
+  val data: T,
   var signature: String? = null,
 )
 
-interface MessageData<T>
-class RfqData(val offeringId: String): MessageData<MessageType.Rfq>
-class OrderData: MessageData<MessageType.Order>
+interface MessageData
+class RfqData(val offeringId: String): MessageData
+class OrderData: MessageData
 
-sealed class MessageType(val typeName: String) {
-  object Rfq : MessageType("rfq")
-  object Order : MessageType("order")
+enum class MessageType {
+  Rfq,
+  Order
 }
 
-abstract class MessageMetadata(private val messageType: MessageType) {
-  abstract val from: String
-  abstract val to: String
+class MessageMetadata(
+  val messageType: MessageType,
+  val from: String,
+  val to: String,
+)
 
-  val type: String
-    get() = messageType.typeName
-}
-class RfqMetadata(override val from: String, override val to: String) : MessageMetadata(MessageType.Rfq)
-class OrderMetadata(override val from: String, override val to: String) : MessageMetadata(MessageType.Order)
-
-class Rfq(metadata: RfqMetadata, data: RfqData): Message<MessageType.Rfq>(metadata, data)
-class Order(metadata: OrderMetadata, data: OrderData): Message<MessageType.Order>(metadata, data)
-
-val rfq = Rfq(RfqMetadata("from", "to"), RfqData("offeringId"))
+val rfq = Message<RfqData>(
+  MessageMetadata(MessageType.Rfq,
+    "from",
+    "to"
+  ),
+  RfqData("offeringId")
+)
