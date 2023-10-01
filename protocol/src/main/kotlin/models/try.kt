87b66fa89eb136db1f4435a847712a0c135dfdbc
Phoebe Lew <plew@squareup.com>
2023-10-01 17:54:50 +1100

    Dump some chad code into try.kt

--- protocol/src/main/kotlin/models/try.kt
+++ protocol/src/main/kotlin/models/try.kt
@@ -1,57 +1,51 @@
 package models
 
-import java.time.Instant
-import java.util.Date
-import java.util.UUID
+import typeid.TypeID
+import java.time.OffsetDateTime
 
+// Abstract Message class with generics
+sealed class Payload
+class IntPayload(val value: Int) : Payload()
+class StringPayload(val value: String) : Payload()
 
-//abstract class ResourceMetadata(val kind: ResourceType) {
-//  abstract val from: String
-//  val id: String
-//  val createdAt: Date = Date() // or kotlinx-datetime
-//  abstract val updatedAt: Date?
-//
-//  init {
-//    this.id = "${kind}_${UUID.randomUUID()}"
-//  }
-//}
-//
-//class OfferingMetadata(
-//  override val from: String,
-//  override val updatedAt: Date?
-//) : ResourceMetadata(ResourceType.Offering)
-//
-//class ReputationMetadata(
-//  override val from: String,
-//  override val updatedAt: Date?
-//) : ResourceMetadata(ResourceType.Reputation)
-//
-//val metadata = OfferingMetadata("from", Date()).kind
+// Abstract Message class with generics
+abstract class Message<TPayload : Payload, TData : Any> {
+  abstract val payload: TPayload // Abstract property
 
+  abstract fun create(data: TData): Message<TPayload, TData>
+}
 
-class ResourceMetadata2(
-  val from: String,
-  val id: String,
-  val createdAt: Instant,
-  val updatedAt: Instant?,
-  val kind: String
-) {
-  constructor(
-    from: String,
-    id: String,
-    createdAt: Instant,
-    updatedAt: Instant?
-  ) : this(from, id, createdAt, updatedAt, mapTypeParameterToKind<String>())
+// Rfq class, a subclass of Message
+class Rfq private constructor(private val rfqData: Int) : Message<IntPayload, Int>() {
+  override val payload: IntPayload = IntPayload(rfqData)
 
   companion object {
-    // Define a mapping from type parameter T to kind value
-    private inline fun <reified T : String> mapTypeParameterToKind(): String {
-      return when (T::class.simpleName) {
-        "Offering" -> "Offering"
-        "Reputation" -> "Reputation"
-        else -> throw IllegalArgumentException("Invalid type parameter: ${T::class.simpleName}")
-      }
+    fun create(data: Int): Rfq {
+      // Parsing and creating an Rfq message
+      return Rfq(data)
     }
   }
 }
+
+// Quote class, a subclass of Message
+class Quote private constructor(private val quoteData: String) : Message<StringPayload, String>() {
+  override val payload: StringPayload = StringPayload(quoteData)
+
+  companion object {
+    fun create(data: String): Quote {
+      // Parsing and creating a Quote message
+      return Quote(data)
+    }
+  }
+}
+
+fun main() {
+  val rfqMessage = Rfq.create(42)
+  val quoteMessage = Quote.create("42.5")
+
+  println("RFQ Payload: ${rfqMessage.payload.value}")
+  println("Quote Payload: ${quoteMessage.payload.value}")
+}
+
+
 
