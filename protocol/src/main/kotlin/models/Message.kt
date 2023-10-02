4f3890af01e1fa0d0b4109b84a61153f91a25ee8
Phoebe Lew <plew@squareup.com>
2023-10-03 10:30:21 +1100

    Pull in tom and Ethan changes

--- protocol/src/main/kotlin/models/Message.kt
+++ protocol/src/main/kotlin/models/Message.kt
@@ -1,195 +1,68 @@
 package models
 
-import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
-import com.fasterxml.jackson.module.kotlin.readValue
-import typeid.TypeID
-import java.time.OffsetDateTime
-import java.util.Date
+// Abstract Message class with generics
+sealed class MessageData
+class RfqData(val amount: Int) : MessageData()
+class OrderData : MessageData()
+//
+//sealed class MessageMetadata
+//class RfqMetadata(): MessageMetadata()
+//class OrderMetadata(): MessageMetadata()
 
-sealed class MessageType(val typeName: String) {
-  object Rfq : MessageType("rfq")
-  object Quote : MessageType("quote")
-  object Order : MessageType("order")
-  object OrderStatus : MessageType("orderstatus") // use camel case or kebab case?
-  object Close : MessageType("close")
-}
+// Abstract Message class with generics
+//abstract class Message<TData : MessageData, TMetadata : Any> {
+////  abstract val payload: TData // Abstract property
+//  abstract companion object{
+//  abstract fun create(data: TData): Message<TData, TMetadata>
+//  abstract fun parse(data: Any): Message<TData, TMetadata>
+//  }
+//}
 
-@Suppress("UNCHECKED_CAST")
-abstract class Message<T : MessageType>(
-  val metadata: MessageMetadata,
-  val data: MessageData<T>,
-  var signature: String? = null,
-) {
+// Rfq class
+class Rfq private constructor(private val rfqData: RfqData) {
+  //  override val payload: IntPayload = IntPayload(rfqData)
+  val metadata: MessageMetadata
+  val data: RfqData
+  var signature: String? = null
 
-  abstract val kind: MessageType
+  fun sign() {
+    this.signature = "blah"
+  }
 
   companion object {
-    val mapper = jacksonObjectMapper()
-    inline fun <reified T : MessageType> parse(message: String): Message<T> {
-      // verify message
+    fun create(data: RfqCreateArgs): Rfq {
 
-      mapper.readValue<Message<MessageType.Rfq>>(message)
-
-      return when (T::class) {
-        Rfq::class -> {
-          return Rfq(
-            RfqMetadata(message.),
-            RfqData("blah", 1)
-          ) as Message<T> // requires suppress unchecked cast, any way around that?
-        }
-
-        Quote::class -> {
-          return Quote(QuoteMetadata(), QuoteData(expiresAt = Date())) as Message<T>
-        }
-
-        Order::class -> {
-          return Order(OrderMetadata(), OrderData()) as Message<T>
-        }
-
-        OrderStatus::class -> {
-          return OrderStatus(OrderStatusMetadata(), OrderStatusData("bleh")) as Message<T>
-        }
-
-        Close::class -> {
-          return Close(CloseMetadata(), CloseData("bleh")) as Message<T>
-        }
-
-        else -> {}
-      }
     }
 
-  }
-
-}
-
-interface MessageData<T>
-class RfqData(
-  val offeringId: String,
-  val payinSubunits: Int,
-//  val payinMethod: SelectedPaymentMethod,
-//  val payoutMethod: SelectedPaymentMethod,
-//  val claims: List<String>
-) : MessageData<MessageType.Rfq>
-
-class SelectedPaymentMethod(val kind: String)
-
-class QuoteData(
-  val expiresAt: Date,
-//  val payin: QuoteDetails,
-//  val payout: QuoteDetails,
-//  val paymentInstructions: PaymentInstructions?,
-) : MessageData<MessageType.Quote>
-
-class QuoteDetails(val currencyCode: String, val amountSubunits: String, val feeSubunits: String?)
-
-class PaymentInstructions(val payin: PaymentInstruction?, val payout: PaymentInstruction?)
-
-class PaymentInstruction(val link: String?, val instruction: String?)
-
-
-class OrderData : MessageData<MessageType.Order>
-
-class OrderStatusData(
-  val orderStatus: String,
-) : MessageData<MessageType.OrderStatus>
-
-class CloseData(
-  val reason: String?,
-) : MessageData<MessageType.Close>
-
-abstract class MessageMetadata(private val messageType: MessageType) {
-  abstract val from: String
-  abstract val to: String
-  abstract val id: TypeID
-  abstract val exchangeId: String
-  abstract val createdAt: OffsetDateTime
-
-  val type: String
-    get() = messageType.typeName
-
-}
-
-class RfqMetadata(override val from: String, override val to: String, override val id: TypeID, override val exchangeId: String, override val createdAt: OffsetDateTime) : MessageMetadata(
-  MessageType.Rfq
-)
-
-class QuoteMetadata(override val from: String, override val to: String, override val id: TypeID, override val exchangeId: String, override val createdAt: OffsetDateTime) : MessageMetadata(
-  MessageType.Quote
-)
-
-class OrderMetadata(override val from: String, override val to: String, override val id: TypeID, override val exchangeId: String, override val createdAt: OffsetDateTime) : MessageMetadata(
-  MessageType.Order
-)
-
-class OrderStatusMetadata(override val from: String, override val to: String, override val id: TypeID, override val exchangeId: String, override val createdAt: OffsetDateTime) : MessageMetadata(
-  MessageType.OrderStatus
-)
-
-class CloseMetadata(override val from: String, override val to: String, override val id: TypeID, override val exchangeId: String, override val createdAt: OffsetDateTime) : MessageMetadata(
-  MessageType.Close
-)
-
-class RfqCreateArgs(
-  val from: String,
-  val to: String,
-  val exchangeId: String,
-  val offeringId: String,
-  val payinSubunits: Int,
-) {
-  val kind = MessageType.Rfq
-}
-
-class Rfq(args: RfqCreateArgs) : Message<MessageType.Rfq>(
-  RfqMetadata(
-    args.from,
-    args.to,
-    TypeID(args.kind.typeName),
-    args.exchangeId,
-    OffsetDateTime.now()
-  ), RfqData(args.offeringId, args.payinSubunits)
-) {
-  override val kind: MessageType = MessageType.Rfq
-
-//    fun create(args: RfqCreateArgs): Message<MessageType.Rfq> {
-//      val metadata = RfqMetadata(args.from, args.to, TypeID(this.), args.exchangeId, OffsetDateTime.now())
-//      val data = RfqData(args.offeringId, args.payinSubunits)
-//      return Rfq(metadata, data)
-//    }
-
-
-}
-
-class Quote(metadata: QuoteMetadata, data: QuoteData) : Message<MessageType.Quote>(metadata, data)
-
-class OrderCreateArgs(
-  val from: String,
-  val to: String,
-  val exchangeId: String,
-)
-
-class Order(metadata: OrderMetadata, data: OrderData) : Message<MessageType.Order>(metadata, data) {
-  override val kind: MessageType = MessageType.Order
-
-  fun create(args: OrderCreateArgs): Message<MessageType.Order> {
-    val metadata = OrderMetadata(args.from, args.to, TypeID(kind.typeName), args.exchangeId, OffsetDateTime.now())
-    val data = OrderData()
-    return Order(metadata, data)
+    fun parse(data: Any): Rfq {
+      TODO("Not yet implemented")
+    }
   }
 }
 
-class OrderStatus(metadata: OrderStatusMetadata, data: OrderStatusData) : Message<MessageType.OrderStatus>(
-  metadata,
-  data
-)
+class Order private constructor() {
+  val metadata: MessageMetadata
+  val data: OrderData
+  var signature: String? = null
 
-class Close(metadata: CloseMetadata, data: CloseData) : Message<MessageType.Close>(metadata, data)
+  companion object {
+    fun create(): Order {
+      return Order()
+    }
 
-// should we make the constructor private and force people to go through a static Create() or Parse() method?
-val rfq = Rfq(RfqCreateArgs(from = "from", to = "to", "exchangeId","offeringId", 0))
-val test = Message.parse<MessageType.Rfq>(object {
-  val test = "test"
-})
+    fun parse(data: Any): Order {
+      TODO("Not yet implemented")
+    }
+  }
+}
 
-// using static create method
-val rfq = Rfq
+fun main() {
+  val rfqMessage = Rfq.create(42)
+  val orderMessage = Order.create("42.5")
+
+  println("RFQ Payload: ${rfqMessage.data.value}")
+  println("Quote Payload: ${orderMessage.data.value}")
+}
+
+
 
--- protocol/src/main/kotlin/models/Resource.kt
+++ protocol/src/main/kotlin/models/Resource.kt
@@ -1,55 +1,40 @@
 package models
 
 import net.pwall.json.schema.JSONSchema
-import java.util.Date
+import java.time.OffsetDateTime
 import java.util.UUID
 
 
-sealed class ResourceKind {
-  class Offering : ResourceKind()
-  class Reputation : ResourceKind()
+enum class ResourceKind {
+  Offering,
+  Reputation
 }
 
-abstract class Resource<T : ResourceKind> {
-  abstract val metadata: ResourceMetadata // this could be out of sync with the type of data
-  abstract val data: ResourceData
-  abstract var signature: String?
-}
-
-class Offering(override val data: OfferingData, override val metadata: ResourceMetadata) : Resource<ResourceKind.Offering>() {
-  override var signature: String? = null
-
-  companion object {
-    fun create(data: OfferingData): Offering {
-      val kind = Resour.Offering // want this to be shared in the whole class and for implementers to have to provide it
-      val metadata = ResourceMetadata(
-        kind,
-        "from",
-        "${kind}_${UUID.randomUUID()}",
-        Date()
-      ) // dont like that the id logic lives here instead of on a base class
-      return Offering(data, metadata)
+class Resource<T : ResourceData>(
+  val metadata: ResourceMetadata, // this could be out of sync with the type of data
+  val data: T,
+  var signature: String?
+) {
+  init {
+    if (metadata.kind != data.kind) {
+      throw IllegalArgumentException("Metadata and data types do not match.")
     }
   }
 }
 
-//abstract class Resource<T: ResourceKind> {
-//  abstract val metadata: ResourceMetadata<T> // this could be out of sync with the type of data
-//  abstract val data: T
-//  abstract var signature: String?
-//}
+class ResourceMetadata(
+  val from: String,
+  val kind: ResourceKind,
+  val id: String = "${kind}_${UUID.randomUUID()}",
+  val createdAt: OffsetDateTime, // or kotlinx-datetime
+  val updatedAt: OffsetDateTime?
+)
 
+sealed interface ResourceData {
+  val kind: ResourceKind
+}
 
-class ReputationMetadata(
-  override val from: String,
-  override val updatedAt: Date?
-) : ResourceMetadata(ResourceType.Reputation)
-
-val metadata = OfferingMetadata("from", Date())
-
-interface ResourceData<T>
-
-class OfferingData(
+class Offering(
   val description: String,
   val payoutUnitsPerPayinUnit: Int,
   val payoutCurrency: CurrencyDetails,
@@ -57,8 +42,9 @@ class OfferingData(
   val payinMethods: List<PaymentMethod>,
   val payoutMethods: List<PaymentMethod>,
   val requiredClaims: PresentationExchange
-) : ResourceData<Offering>
-
+) : ResourceData {
+  override val kind = ResourceKind.Offering
+}
 
 class CurrencyDetails(
   val currencyCode: String,
@@ -71,60 +57,23 @@ class PaymentMethod(
   val requiredPaymentDetails: JSONSchema
 )
 
-//class Offering(
-//  override val metadata: OfferingMetadata,
-//  override val data: OfferingData,
-//  override val signature: String
-//) : Resource()
+class PresentationExchange
 
-abstract class ResourceMetadata {
-  abstract val from: String
-  abstract val kind: String
-  val id: String = "${kind}_${UUID.randomUUID()}"
-  abstract val createdAt: Date // or kotlinx-datetime
-  abstract val updatedAt: Date?
-
-}
-
-class Offering2 {
-  private val metadata: OfferingMetadata
-  private val data: OfferingData
-  private var signature: String? = null
-
-  private constructor(metadata: OfferingMetadata, data: OfferingData) {
-    this.metadata = metadata
-    this.data = data
-  }
-
-  companion object {
-    fun create(from: String, id: String, data: OfferingData): Offering2 {
-      val metadata = OfferingMetadata(from)
-      return
-    }
-  }
-
-  class OfferingMetadata(override val from: String, override val createdAt: Date, override val updatedAt: Date?) : ResourceMetadata() {
-    override val kind: String = "offering"
-
-  }
-}
-
-class Resource3<T>(val metadata: ResourceMetadata3<T>, // this could be out of sync with the type of data
-                   val data: ResourceData<T>,
-                   var signature: String?) {
-  companion object {
-    fun <T> parse(object: Any): Resource3<T> {
-
-    }
-  }
-}
-
-class ResourceMetadata3<T>(
-  val from: String,
-  val id: String,
-  val createdAt: Date, // or kotlinx-datetime
-  val updatedAt: Date? = null
-) {
-}
-
-class ResourceData3<T>
+val offering = Resource(
+  metadata = ResourceMetadata(
+    from = "from",
+    kind = ResourceKind.Offering,
+    createdAt = OffsetDateTime.now(),
+    updatedAt = OffsetDateTime.now()
+  ),
+  data = Offering(
+    description = "",
+    payoutUnitsPerPayinUnit = 1,
+    payinCurrency = CurrencyDetails("", "", ""),
+    payoutCurrency = CurrencyDetails("", "", ""),
+    payinMethods = listOf(),
+    payoutMethods = listOf(),
+    requiredClaims = PresentationExchange()
+  ),
+  signature = null
+)
