37717a3c8794efb5b6668c0cee24814cde79b003
Phoebe Lew <plew@squareup.com>
2023-10-04 21:44:04 +1100

    Add assertion for message parsing

--- protocol/src/test/kotlin/models/MessageTest.kt
+++ protocol/src/test/kotlin/models/MessageTest.kt
@@ -2,6 +2,8 @@ package protocol.models
 
 import com.fasterxml.jackson.core.JsonParseException
 import models.Message
+import models.Order
+import models.Rfq
 import org.junit.jupiter.api.assertThrows
 import protocol.TestData
 import kotlin.test.Test
@@ -20,13 +22,14 @@ class MessageTest {
   fun `can parse a list of messages`() {
     val rfq = TestData.getRfq()
     val order = TestData.getOrder()
+    val messages = listOf(rfq.toJsonString(), order.toJsonString()).map { Message.parse(it) }
 
-    val exchange = listOf(rfq.toJsonString(), order.toJsonString())
+    assertIs<Rfq>(messages.first())
+    assertIs<Order>(messages.last())
   }
 
   @Test
   fun `parse throws error if json string is not valid`() {
     assertThrows<JsonParseException> { Message.parse(";;;;") }
   }
-
 }
