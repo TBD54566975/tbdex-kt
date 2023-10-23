a6006198390ca5ef82ced72adf98d92b573e66c0
Moe Jangda <moe@tbd.email>
2023-10-23 00:02:34 -0700

    remove unused imports

--- protocol/src/main/kotlin/tbdex/sdk/protocol/serialization/Json.kt
+++ protocol/src/main/kotlin/tbdex/sdk/protocol/serialization/Json.kt
@@ -5,8 +5,6 @@ import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.ObjectWriter
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.module.kotlin.registerKotlinModule
-import tbdex.sdk.protocol.Json.stringify
-import tbdex.sdk.protocol.serialization.TypeIdModule
 
 const val dateTimeFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSXXX"
 
