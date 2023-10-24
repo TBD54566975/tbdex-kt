37c49f58dcad880a4c24d3ea6ab32e878587eb0a
Phoebe Lew <plew@squareup.com>
2023-10-23 17:18:12 -0700

    Disable tests

--- httpclient/src/test/kotlin/tbdex/sdk/httpclient/YellowcardTest.kt
+++ httpclient/src/test/kotlin/tbdex/sdk/httpclient/YellowcardTest.kt
@@ -1,6 +1,7 @@
 package tbdex.sdk.httpclient
 
-import GetOfferingsResponse
+import org.junit.jupiter.api.Disabled
+import tbdex.sdk.httpclient.models.GetOfferingsResponse
 import tbdex.sdk.protocol.models.Offering
 import tbdex.sdk.protocol.models.Rfq
 import tbdex.sdk.protocol.models.RfqData
@@ -8,16 +9,19 @@ import tbdex.sdk.protocol.models.SelectedPaymentMethod
 import kotlin.test.Test
 
 class YellowCardTest {
+  @Suppress("MaximumLineLength")
   private val ionDid =
     "did:ion:EiDCYKaMtz8hWnylrPKaDsOqNoM973GWqfGCUIeLQesWcg:eyJkZWx0YSI6eyJwYXRjaGVzIjpbeyJhY3Rpb24iOiJyZXBsYWNlIiwiZG9jdW1lbnQiOnsicHVibGljS2V5cyI6W3siaWQiOiJkd24tc2lnIiwicHVibGljS2V5SndrIjp7ImNydiI6IkVkMjU1MTkiLCJrdHkiOiJPS1AiLCJ4IjoidGdXWUF3ajlSRkhXaEJON2Fya0pnQTJKSUlDbHg2Zm54cjVjeE9jNm95SSJ9LCJwdXJwb3NlcyI6WyJhdXRoZW50aWNhdGlvbiJdLCJ0eXBlIjoiSnNvbldlYktleTIwMjAifV0sInNlcnZpY2VzIjpbeyJpZCI6InBmaSIsInNlcnZpY2VFbmRwb2ludCI6Imh0dHBzOi8vcGZpLnllbGxvd2NhcmQuZW5naW5lZXJpbmciLCJ0eXBlIjoiUEZJIn1dfX1dLCJ1cGRhdGVDb21taXRtZW50IjoiRWlCSk9ha3M4WmI2LXJueDdzMERnWnZqel9YS3NfUEJoN3BTcUgycUQzMXphQSJ9LCJzdWZmaXhEYXRhIjp7ImRlbHRhSGFzaCI6IkVpQWQxRTRSWVBEdlUtTUNlZnY3cUZUOEszaTVZcjNrZ3BnOWhiSkhsWXg0ZnciLCJyZWNvdmVyeUNvbW1pdG1lbnQiOiJFaUNXYzVzekFiWUpsMzVWci1Sdzl6ZE1hWDNlaGZPQUlBUHhEVnhsY3NjWWZBIn19"
 
   @Test
+  @Disabled
   fun `can get offerings`() {
     val resp = RealTbdexClient.getOfferings(ionDid) as GetOfferingsResponse
     // TODO assert response
   }
 
   @Test
+  @Disabled
   fun `can send an RFQ`() {
     val resp = RealTbdexClient.getOfferings(ionDid) as GetOfferingsResponse
     val offering = resp.data[2] as Offering
@@ -38,6 +42,7 @@ class YellowCardTest {
   }
 
   @Test
+  @Disabled
   fun `can get an exchange`() {
     TODO()
   }
