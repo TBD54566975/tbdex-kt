3873e2cb0b68255c4a6bbd992fe91f5d271311cf
Moe Jangda <moe@tbd.email>
2023-10-24 01:19:20 -0700

    default `assertionMethods` to an empty list if null

--- protocol/src/main/kotlin/tbdex/sdk/protocol/CryptoUtils.kt
+++ protocol/src/main/kotlin/tbdex/sdk/protocol/CryptoUtils.kt
@@ -51,7 +51,7 @@ object CryptoUtils {
     // or just `#fragment`. See: https://www.w3.org/TR/did-core/#relative-did-urls.
     // Using a set for fast string comparison. DIDs can be long.
     val verificationMethodIds = setOf(parsedDidUrl.didUrlString, "#${parsedDidUrl.fragment}")
-    val assertionMethods = didResolutionResult.didDocument.assertionMethodVerificationMethodsDereferenced
+    val assertionMethods = didResolutionResult.didDocument.assertionMethodVerificationMethodsDereferenced ?: listOf()
     var assertionMethod: VerificationMethod? = null
 
     for (method in assertionMethods) {
