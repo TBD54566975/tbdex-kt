ee70b3685f3211ad2b8cfcfdf5005c2a88d1fe62
Phoebe Lew <plew@squareup.com>
2023-10-30 16:42:28 -0700

    WIP

--- typeid/src/test/kotlin/tbdex/sdk/typeid/TypeIdTest.kt
+++ typeid/src/test/kotlin/tbdex/sdk/typeid/TypeIdTest.kt
@@ -1,10 +1,29 @@
 package tbdex.sdk.typeid
 
+import com.fasterxml.jackson.core.type.TypeReference
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.dataformat.yaml.YAMLFactory
+import org.junit.jupiter.api.Test
 
 class TypeIdTest {
   private val yamlMapper: ObjectMapper = ObjectMapper(YAMLFactory())
 
-  
+  data class Valid(val name: String, val prefix: String, val uuid: String)
+  data class Invalid(val name: String, val typeId: String, val description: String)
+
+
+  @Test
+  fun `test valid`() {
+    yamlMapper.readValue(
+      TypeIdTest::class.java.getResourceAsStream("/valid.yml"), object : TypeReference<List<Valid>>() {})
+      .stream()
+  }
+
+  @Test
+  fun `test invalid`() {
+    yamlMapper.readValue(
+      TypeIdTest::class.java.getResourceAsStream("/invalid.yml"), object : TypeReference<List<Valid>>() {})
+      .stream()
+
+  }
 }
