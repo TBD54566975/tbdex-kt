f38ead361a51ff5bcdaf25c44118d01056953a44
Andres Uribe Gonzalez <auribe@squareup.com>
2023-11-20 12:19:43 -0500

    Use the v for tags, and use DNS over HTTPS

--- httpclient/build.gradle.kts
+++ httpclient/build.gradle.kts
@@ -15,18 +15,21 @@ repositories {
   }
 }
 
+val okHttpVersion = "4.9.1"
+
 dependencies {
   implementation(project(":protocol"))
   implementation("de.fxlae:typeid-java-jdk8:0.2.0")
-  implementation("com.squareup.okhttp3:okhttp:4.9.1")
+  implementation("com.squareup.okhttp3:okhttp:$okHttpVersion")
+  implementation("com.squareup.okhttp3:okhttp-dnsoverhttps:$okHttpVersion")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.9.8")
   implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-  implementation("com.github.TBD54566975:web5-kt:0.0.9-beta")
+  implementation("com.github.TBD54566975:web5-kt:v0.0.9-beta")
   implementation("decentralized-identity:did-common-java:1.9.0") // would like to grab this via web5 dids
 
   testImplementation(kotlin("test"))
-  testImplementation("com.squareup.okhttp3:mockwebserver:4.11.0")
+  testImplementation("com.squareup.okhttp3:mockwebserver:$okHttpVersion")
   testImplementation("com.willowtreeapps.assertk:assertk:0.27.0")
 
 }
