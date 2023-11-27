6c6f5e7ee892c1819fb1307b554bdfac237f567e
Kendall Weihe <kendallweihe@gmail.com>
2023-11-27 11:44:02 -0500

    Fix web5-kt version tag & broken schema links (#108)
    * Fix web5-kt version tag
    
    * Bump tbdex version to 0.6.0
    
    * Fix tests, broken schema urls

--- httpclient/build.gradle.kts
+++ httpclient/build.gradle.kts
@@ -22,7 +22,7 @@ dependencies {
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.9.8")
   implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-  implementation("com.github.TBD54566975:web5-kt:0.0.9-beta")
+  implementation("com.github.TBD54566975:web5-kt:v0.0.9-beta")
   implementation("decentralized-identity:did-common-java:1.9.0") // would like to grab this via web5 dids
 
   testImplementation(kotlin("test"))
