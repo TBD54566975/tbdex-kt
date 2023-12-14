d78a4ae00c47e418bff116813ea6c3143d61a30c
Phoebe Lew <plew@squareup.com>
2023-12-14 16:08:12 +1100

    Test changes

--- httpserver/src/test/kotlin/tbdex/sdk/httpserver/handlers/SubmitRfqTest.kt
+++ httpserver/src/test/kotlin/tbdex/sdk/httpserver/handlers/SubmitRfqTest.kt
@@ -1,68 +1,45 @@
 package tbdex.sdk.httpserver.handlers
 
-import io.ktor.server.testing.TestApplication
-import io.ktor.server.testing.testApplication
 import okhttp3.OkHttpClient
 import okhttp3.Request
 import okhttp3.RequestBody.Companion.toRequestBody
-import org.junit.After
 import org.junit.jupiter.api.AfterAll
 import org.junit.jupiter.api.BeforeAll
 import org.junit.jupiter.api.Test
 import tbdex.sdk.httpserver.TbdexHttpServer
 import tbdex.sdk.httpserver.TbdexHttpServerConfig
+import kotlin.concurrent.thread
 
-class SubmitRfqTest {
-  val api = TbdexHttpServer(TbdexHttpServerConfig(8080))
+class SubmitRfqTest : TbdexHttpServer() {
   val client = OkHttpClient()
-
-  @After
-  fun teardown() {
-    api.stop()
-  }
 
   @Test
   fun `returns 400 if no request body is provided`() {
-    api.start()
-
     val request = Request.Builder()
-      .url("http://localhost:8000/exchanges/123/rfq")
+      .url("http://10.0.2.2:8080/exchanges/123/rfq")
       .post("".toRequestBody())
       .build()
 
     val response = client.newCall(request).execute()
+
   }
 
-//  @Test
-//  fun `start server`() {
-//    testApplication {
-//      application {
-//        module()
-//      }
-//    }(api.server) {
-//      handleRequest(HttpMethod.Post, "/").apply {
-//        assertContains("Please use the tbdex protocol", response.content!!)
-//      }
-//    }
-//  }
-
   companion object {
-    lateinit var testApp: TestApplication
+    private lateinit var server: TbdexHttpServer
 
     @JvmStatic
     @BeforeAll
     fun setup() {
-      testApp = TestApplication {
-        testApplication {
-          TbdexHttpServer.module
-        }
+      thread {
+        server = TbdexHttpServer(TbdexHttpServerConfig(8080))
+        server.start()
       }
     }
 
     @JvmStatic
     @AfterAll
     fun teardown() {
-      testApp.stop()
+      server.stop()
     }
   }
 }
