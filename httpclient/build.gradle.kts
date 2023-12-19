36bc787e1a9ef87b113fb3df3475b4248efdd954
Kendall Weihe <kendallw@squareup.com>
2023-12-19 08:28:56 -0500

    Fix httpserver web5 dependency

--- httpclient/build.gradle.kts
+++ httpclient/build.gradle.kts
@@ -19,13 +19,14 @@ repositories {
 }
 
 dependencies {
+  api("de.fxlae:typeid-java-jdk8:0.2.0")
+  api("xyz.block:web5:0.0.9-delta")
+
   implementation(project(":protocol"))
-  implementation("de.fxlae:typeid-java-jdk8:0.2.0")
   implementation("com.squareup.okhttp3:okhttp:4.9.1")
   implementation("com.fasterxml.jackson.module:jackson-module-kotlin:2.14.2")
   implementation("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.9.8")
   implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
-  implementation("xyz.block:web5:0.0.9-delta")
   implementation("decentralized-identity:did-common-java:1.9.0") // would like to grab this via web5 dids
 
   testImplementation(kotlin("test"))
