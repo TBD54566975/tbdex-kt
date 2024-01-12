08ff9d48e7af8669d925bbc9c78d0ce01083facf
Phoebe Lew <plew@squareup.com>
2024-01-12 18:20:13 +1100

    Add gradle task

--- protocol/src/test/kotlin/tbdex/sdk/protocol/TbdexTestVectorsMessageParse.kt
+++ protocol/src/test/kotlin/tbdex/sdk/protocol/TbdexTestVectorsMessageParse.kt
@@ -11,7 +11,7 @@ import tbdex.sdk.protocol.serialization.Json
 import kotlin.test.assertEquals
 import kotlin.test.assertIs
 
-class MessagesVectorTest {
+class TbdexTestVectorsMessageParse {
   @Test
   fun `parse rfq`() {
     testParsing<Rfq>(TestVectors.rfq())
