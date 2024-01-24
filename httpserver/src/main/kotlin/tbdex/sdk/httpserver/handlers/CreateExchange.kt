d7b968c346d7533a743a1c21f87e70599f5cf32a
Jiyoon Koo <jiyoon@squareup.com>
2024-01-24 12:27:34 -0500

    Update httpserver/src/main/kotlin/tbdex/sdk/httpserver/handlers/CreateExchange.kt
    Co-authored-by: Diane Huxley <dhuxley@tbd.email>

--- httpserver/src/main/kotlin/tbdex/sdk/httpserver/handlers/CreateExchange.kt
+++ httpserver/src/main/kotlin/tbdex/sdk/httpserver/handlers/CreateExchange.kt
@@ -44,7 +44,7 @@ suspend fun createExchange(
     val rfqJsonString = jsonNode["rfq"].toString()
 
     message = Message.parse(rfqJsonString) as Rfq
-    replyTo = jsonNode["replyTo"]?.takeIf { !it.isNull }?.asText()
+    replyTo = jsonNode["replyTo"]?.asText()
 
   } catch (e: Exception) {
     val errorDetail = ErrorDetail(detail = "Parsing of TBDex message failed: ${e.message}")
